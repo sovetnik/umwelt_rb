c93dc2156b775ab473030d7396f67adafead5a51
Oleg Sovetnik <sovetnik@oblaka.biz>
2019-02-20 23:07:58 +0300

    Add node enumerator to Trunk
    Update faker

--- lib/umwelt/tree/trunk.rb
+++ lib/umwelt/tree/trunk.rb
@@ -8,6 +8,13 @@ module Umwelt::Tree
       @childs_ids = childs_ids
     end
 
+    def each_node(&block)
+      @index
+        .values
+        .map { |fragment| builder.call(fragment) }
+        .each(&block)
+    end
+
     def node(id)
       builder.call(@index[id])
     end
